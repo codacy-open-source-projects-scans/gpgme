2e482f0b149c3e12b7cc62ed92314cce25dea94f
Codacy Archive <codacy-archive@local>
2025-12-15 09:45:34 +0000

    snapshot 2025-12-15T09:45:34.125637+00:00

--- src/gpgme-w32spawn.c
+++ src/gpgme-w32spawn.c
@@ -38,6 +38,7 @@
 #include <stdint.h>
 #include <process.h>
 
+#include "util.h"
 #include "priv-io.h"
 
 /* #define DEBUG_TO_FILE 1 */
@@ -249,7 +250,7 @@ my_spawn (wchar_t **argv, struct spawn_fd_item_s *fd_list, unsigned int flags)
           handle = LoadLibraryA ("user32.dll");
           if (handle)
             {
-              func = GetProcAddress (handle, "AllowSetForegroundWindow");
+              func = (void *)GetProcAddress (handle, "AllowSetForegroundWindow");
               if (!func)
                 FreeLibrary (handle);
             }
